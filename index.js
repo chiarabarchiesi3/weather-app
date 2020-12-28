ab75d54428c1b63b84722a46900ef9cfd56666a0
Chiara Barchiesi <chiarabarchiesi3@gmail.com>
2020-12-28 19:33:53 +0100

    update time

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ let now = new Date();
 let dateElement = document.querySelector("#date");
 let date = now.getDate();
 let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 let year = now.getFullYear();
-
 let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 let day = days[now.getDay()];
 
